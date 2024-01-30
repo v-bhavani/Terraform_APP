dce141741b7fd480a327196017165c9c4a861634
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-01-30 18:17:11 +0530

    Update output.tf

--- Create_VM/output.tf
+++ Create_VM/output.tf
@@ -1,11 +1,6 @@
 output "vm_id" {
   value = azurerm_linux_virtual_machine.main.id
 }
-
-output "vm_private_ip" {
-  value = azurerm_network_interface.vm_nic.private_ip_address
-}
-
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
 }
