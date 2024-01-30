6c20c5ef27c410d1091c7a9572e58b7ae12d873e
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-01-30 18:20:58 +0530

    Update output.tf

--- Create_VM/output.tf
+++ Create_VM/output.tf
@@ -1,6 +1,6 @@
-output "vm_id" {
-  value = azurerm_linux_virtual_machine.main.id
-}
+#output "vm_id" {
+#  value = azurerm_linux_virtual_machine.main.id
+#}
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
 }
