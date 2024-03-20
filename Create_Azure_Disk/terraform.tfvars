3a3464c83aa011578c1f4faa89307b9bc3127616
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-03-20 10:22:37 +0530

    Add files via upload

---
+++ Create_Azure_Disk/terraform.tfvars
@@ -0,0 +1,8 @@
+resourcegroup = "$var.rg"
+location = "$var.location"
+diskname = "$var.diskname"
+st_acc_type = "Standard_LRS"
+disksize = $var.disksize
+Owner    = "$var.owner"
+Project  = "$var.project"
+Duration = "$var.duration"
---
+++ Create_Azure_Disk/variables.tf
@@ -0,0 +1,24 @@
+variable "resourcegroup" {
+  type        = string
+}
+variable "location" {
+  type        = string
+}
+variable "diskname" {
+  type        = string
+}
+variable "st_acc_type" {
+  type        = string
+}
+variable "disksize" {
+  type        = number
+}
+variable "Owner" {
+  type        = string
+}
+variable "Project" {
+  type        = string
+}
+variable "Duration" {
+  type        = string
+}
