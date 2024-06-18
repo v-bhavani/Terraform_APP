58a325a38bdbbd298a32257f65afefb1f61f75a7
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-06-18 16:31:32 +0530

    Update outputs.tf

--- AWS_Create_AMI/outputs.tf
+++ AWS_Create_AMI/outputs.tf
@@ -2,5 +2,5 @@ output "instance_state" {
   value = data.aws_instance.existing_instance.instance_state
 }
 output "ami_id" {
-  value = aws_ami_from_instance.example.source_instance_id
+  value = aws_ami_from_instance.example.id
 }
