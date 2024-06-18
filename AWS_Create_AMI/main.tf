db004e56cb206c72152c428dbb45d76cfb6cfb07
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-06-18 14:33:35 +0530

    Update main.tf

--- AWS_Create_AMI/main.tf
+++ AWS_Create_AMI/main.tf
@@ -1,1 +1,25 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+# S3 backend configuration for state storage
+terraform {
+  backend "s3" {
+    bucket         = "testanisble"
+    key            = "tf/stateami.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks1"
+  }
+}
+
+# Fetch details about the existing instance
+data "aws_instance" "existing_instance" {
+  instance_id = var.instance_id
+}
+
+# Create an AMI from the existing instance
+resource "aws_ami_from_instance" "example" {
+  name               = var.ami_name
+  source_instance_id = var.instance_id
+}
