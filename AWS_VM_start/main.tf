9735277f5c2c4b1687d8096efe2183c44b4a6c01
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-06-18 12:30:20 +0530

    Update main.tf

--- AWS_VM_start/main.tf
+++ AWS_VM_start/main.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   region  = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-bucket-bcs-demo"
+    key            = "tf/terraformstart.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
 data "aws_instance" "existing_instance" {
   instance_id = var.instance_id
 }
