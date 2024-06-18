4e508ff8c11beac422ac65e9accad5cc7cf56ed8
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-06-18 14:57:05 +0530

    Delete AWS_VM_stop directory

--- AWS_VM_stop/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "instance_state" {
-  value = data.aws_instance.existing_instance.instance_state
-}
-output "ami_id" {
-  value = aws_ami_from_instance.instance_ami_snapshot.id
-}
--- AWS_VM_stop/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-instance_id = "$var.instanceid"  # Replace with your actual instance ID
-#access_key = "$var.access_key"
-#secret_key = "$var.secret_key"
