47530bdec3db16a45ab09c4913df11caa659ce17
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-06-18 14:34:11 +0530

    Update variable.tf

--- AWS_Create_AMI/variable.tf
+++ AWS_Create_AMI/variable.tf
@@ -1,1 +1,15 @@
+variable "aws_region" {
+  description = "The AWS region where the instance is running"
+  type        = string
+}
+
+variable "instance_id" {
+  description = "The ID of the instance to create an AMI from"
+  type        = string
+}
+
+variable "ami_name" {
+  description = "The name for the new AMI"
+  type        = string
+}
 
