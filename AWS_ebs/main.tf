4641b542157710be4ffcbfa2b72a8af6ed062c00
BCStest <132462815+Irfanaj@users.noreply.github.com>
2024-06-19 19:14:09 +0530

    Update main.tf

--- AWS_ebs/main.tf
+++ AWS_ebs/main.tf
@@ -21,7 +21,7 @@ resource "aws_ebs_volume" "example_volume" {
 }
 
 resource "aws_volume_attachment" "attach_volume" {
-  device_name = "/dev/sdn"  # The device name to expose to the instance
+  device_name = "/dev/sdi"  # The device name to expose to the instance
   instance_id = var.instance_id
   volume_id   = aws_ebs_volume.example_volume[var.volume_to_attach].id
   force_detach = true  # Optional, if you want to force detach the volume if it's already attached
