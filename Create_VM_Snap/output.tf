2cb161f16543365479168688975877e525527c68
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-10-16 10:50:35 +0530

    Update output.tf

--- Create_VM_Snap/output.tf
+++ Create_VM_Snap/output.tf
@@ -29,3 +29,27 @@ output "virtual_machine_name" {
 output "boot_diagnostics_storage_uri" {
   value = azurerm_virtual_machine.myterraformvm.boot_diagnostics.0.storage_uri
 }
+output "disk_id" {
+  value = azurerm_managed_disk.example.id
+}
+output "disk_name" {
+  value = azurerm_managed_disk.example.name
+}
+output "disk_location" {
+  value = azurerm_managed_disk.example.location
+}
+output "disk_resource_group" {
+  value = azurerm_managed_disk.example.resource_group_name
+}
+output "disk_storage_account_type" {
+  value = azurerm_managed_disk.example.storage_account_type
+}
+output "disk_create_option" {
+  value = azurerm_managed_disk.example.create_option
+}
+output "disk_size_gb" {
+  value = azurerm_managed_disk.example.disk_size_gb
+}
+output "disk_tags" {
+  value = azurerm_managed_disk.example.tags
+}
