f1b3b73e31eae771682aa8a9bf89667ad477d0fb
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2026-03-05 09:43:07 +0530

    Update main.tf

--- Create_VM_Snap/main.tf
+++ Create_VM_Snap/main.tf
@@ -7,86 +7,37 @@ terraform {
     resource_group_name = "cloud-demo"
     storage_account_name = "testcloud001423"
     container_name = "terraform"
-    key = "terraformtest.tfstate"
+    key = "terraformvm.tfstate"
  }
 }
 
-resource "azurerm_managed_disk" "copy" {
-  name = "azurevm-os-disk"
-  location = var.location
-  resource_group_name = var.RG
-  storage_account_type = var.disktype
-  create_option = "Copy"
-  source_resource_id = var.snapuri
-  disk_size_gb = var.osdisksize
+# Get existing Resource Group
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
-# Define the existing virtual network and subnet
-data "azurerm_subnet" "existing_subnet" {
-  name                 = var.subnetname
-  virtual_network_name = var.vnetname
-  resource_group_name  = var.RG
+# Get existing NIC
+data "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  resource_group_name = var.resource_group_name
 }
 
-# Create a public IP address
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "azurevm-public-ip"
-  location            = var.location
-  resource_group_name = var.RG
-  allocation_method   = "Dynamic"
-  sku                 = "Basic" 
-  tags = {
-    "Project" = "${var.project}"
-    "Duration" = "${var.duration}"
-    "Owner" = "${var.owner}"
-  }
+resource "azurerm_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  network_interface_ids = [data.azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
+
+  storage_os_disk {
+    name            = var.os_disk_name
+    managed_disk_id = var.os_disk_id
+    create_option   = "Attach"
+    caching         = "ReadWrite"
+   } 
+
+  lifecycle {
+    prevent_destroy = true
+   }
 }
-# Define the network interface for the VM and attach the public IP
-resource "azurerm_network_interface" "vm_nic" {
-  name                = var.vm_names
-  location            = var.location
-  resource_group_name = var.RG
-
-  ip_configuration {
-    name                          = var.vm_names
-    subnet_id                     = data.azurerm_subnet.existing_subnet.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = var.ip
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
-  }
-  tags = {
-    "Project" = "${var.project}"
-    "Duration" = "${var.duration}"
-    "Owner" = "${var.owner}"
-  }
-}
-
-# Create virtual machine
-resource "azurerm_virtual_machine" "myterraformvm" {
-    name                  = var.vm_names
-    location              = var.location
-    resource_group_name   = var.RG
-    network_interface_ids = [azurerm_network_interface.vm_nic.id]
-    vm_size               = var.vm_size
-
-
-    storage_os_disk {
-    name              = azurerm_managed_disk.copy.name
-    os_type           = "Linux"
-    managed_disk_id   = azurerm_managed_disk.copy.id
-    disk_size_gb      = var.osdisksize
-    create_option     = "Attach"
-    }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
- boot_diagnostics {
-    enabled = "true"
-    storage_uri = var.bootdiagnostic
-  }
-}
-
-
-
+ 
