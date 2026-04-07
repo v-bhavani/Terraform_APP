6a36accf9a7cdd239e8940b7514e084e1326e3b6
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2026-04-07 20:08:07 +0530

    Update variables.tf

--- AKS_cluster_upgrade/variables.tf
+++ AKS_cluster_upgrade/variables.tf
@@ -22,3 +22,27 @@ variable "location" {
   description = "AKS Cluster Location"
   type        = string
 }
+variable "oidc_issuer_enabled" {
+  description = "Enable OIDC Issuer for AKS Cluster"
+  type        = bool
+}
+variable "cluster_version" {
+  description = "AKS Cluster Kubernetes Version"
+  type        = string
+}
+variable "node_pool_name" {
+  description = "AKS Cluster Node Pool Name"
+  type        = string
+}
+variable "node_count" {
+  description = "Number of nodes in the default node pool"
+  type        = number
+}
+variable "vm_size" {
+  description = "VM size for the default node pool"
+  type        = string
+}
+variable "os_disk_size_gb" {
+  description = "OS disk size in GB for the default node pool"
+  type        = number
+}
